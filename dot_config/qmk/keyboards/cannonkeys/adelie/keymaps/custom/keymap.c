acf90341ef23b94db16dd9f7ca5cdbebd1f1dc82
Ryan Byon <ryan.h.byon@gmail.com>
2024-02-11 00:49:58 -0800

    feat(keyboard): minor modifications

--- dot_config/qmk/keyboards/cannonkeys/adelie/keymaps/custom/keymap.c
+++ dot_config/qmk/keyboards/cannonkeys/adelie/keymaps/custom/keymap.c
@@ -2,12 +2,14 @@
 
 enum layer_names {
     _BASE,
+    _NUM,
+    _C,
+    _FN,
+    _NO,
     _COLEMAK,
     _QWERTY,
-    _NUM,
     _CODE,
     _NAV,
-    _FN,
 };
 
 enum custom_keycodes {
@@ -32,6 +34,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,   KC_LOPT, KC_LCMD,          SPC_NUM,          KC_SPC,            KC_HYPR, KC_NO,   KC_ROPT,                   KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
+  [_NUM] = LAYOUT_all(
+    TAB_NAV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,     KC_BSPC, DEL_W,  KC_BSLS,          KC_NO,
+    MO(_C),    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_NO,   KC_NO,   KC_4,     KC_5,    KC_6,    KC_NO,    KC_NO,   KC_ENT,                   KC_NO,
+    KC_NO,     KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_NO,   KC_1,    KC_2,     KC_3,    KC_DOT,  HYP_SLSH, KC_RSFT,                  KC_UP,   KC_NO,
+    KC_NO,     KC_NO,   KC_LCMD,          KC_TRNS,          KC_0,              KC_NO,   KC_NO,   KC_NO,                     KC_LEFT, KC_DOWN, KC_RIGHT
+  ),
+
+  [_FN] = LAYOUT_all(
+    QK_BOOT,   KC_BRID, KC_BRIU, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,            KC_SLEP,
+    KC_NO,     KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,                    KC_NO,
+    KC_NO,     KC_VOLD, KC_MUTE, KC_VOLU, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,                    KC_UP,   KC_TRNS,
+    KC_NO,     KC_NO,   KC_NO,            KC_NO,            RGB_TOG,           KC_NO,   KC_NO,   KC_NO,                     KC_LEFT, KC_DOWN, KC_RIGHT
+  ),
+
+  [_C] = LAYOUT_all(
+    KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   S(KC_P),  KC_NO,   KC_NO,  KC_NO,            KC_NO,
+    KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,                    KC_NO,
+    KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,                    KC_NO,   KC_NO,
+    KC_NO,     KC_NO,   KC_NO,            KC_NO,            KC_NO,             KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO
+  ),
+
+  [_NO] = LAYOUT_all(
+    KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,            KC_NO,
+    KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,                    KC_NO,
+    KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,                    KC_NO,   KC_NO,
+    KC_NO,     KC_NO,   KC_NO,            KC_NO,            KC_NO,             KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO
+  ),
+
+  // for devices without Karabiner application.
   [_COLEMAK] = LAYOUT_all(
     TAB_NAV,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,     KC_U,    KC_Y,    KC_SCLN,  KC_BSPC, DEL_W,  KC_BSLS,          TG(_COLEMAK),
     ESC_CODE,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,     KC_E,    KC_I,    KC_O,     KC_QUOT, KC_ENT,                   KC_NO,
@@ -49,29 +80,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_CODE] = LAYOUT_all(
     KC_NO,     C(KC_Q), C(KC_W), C(KC_F), C(KC_P), C(KC_B), KC_NO,   KC_LCBR,  KC_RCBR, KC_PLUS, KC_COLN,  KC_BSPC, DEL_W,  KC_BSLS,          KC_NO,
     KC_TRNS,   C(KC_A), C(KC_R), C(KC_S), C(KC_T), C(KC_G), KC_UNDS, KC_LPRN,  KC_RPRN, KC_EQL,  KC_MINUS, KC_DQUO, KC_ENT,                   KC_NO,
-    KC_NO,     C(KC_Z), C(KC_X), C(KC_C), C(KC_D), C(KC_V), KC_NO,   KC_LBRC,  KC_RBRC, KC_LT,   KC_GT,    KC_QUES,                  KC_UP,   KC_NO,  
+    KC_NO,     C(KC_Z), C(KC_X), C(KC_C), C(KC_D), C(KC_V), KC_LBRC, KC_RBRC,  KC_LT,   KC_GT,   KC_QUES,  KC_NO,                    KC_UP,   KC_NO,
     KC_NO,     KC_NO,   KC_LCMD,          KC_NO,            KC_NO,             KC_NO,   KC_NO,   KC_NO,                     KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
   [_NAV] = LAYOUT_all(
     KC_TRNS,   KC_LSFT, WORD_R,  KC_NO,   KC_NO,   WORD_L,  KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_BSPC, DEL_W,  KC_BSLS,          KC_NO,
     KC_NO,     KC_LCTL, KC_LOPT, KC_LCMD, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, KC_TILD,  KC_GRV,  KC_ENT,                   KC_NO,
-    KC_NO,     G(KC_Z), G(KC_X), G(KC_C), G(KC_V), KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_PIPE,  KC_BSLS,                  KC_UP,   KC_NO,  
+    KC_NO,     G(KC_Z), G(KC_X), G(KC_C), G(KC_V), KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_PIPE,  KC_BSLS,                  KC_UP,   KC_NO,
     KC_NO,     KC_NO,   KC_NO,            KC_NO,            KC_NO,             KC_NO,   KC_NO,   KC_NO,                     KC_LEFT, KC_DOWN, KC_RIGHT
-  ),
-  
-  [_NUM] = LAYOUT_all(
-    TAB_NAV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,     KC_BSPC, DEL_W,  KC_BSLS,          KC_NO,
-    KC_TRNS,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_NO,   KC_NO,   KC_4,     KC_5,    KC_6,    KC_NO,    KC_NO,   KC_ENT,                   KC_NO,
-    KC_NO,     KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_NO,   KC_1,    KC_2,     KC_3,    KC_DOT,  HYP_SLSH, KC_RSFT,                  KC_UP,   KC_NO,  
-    KC_NO,     KC_NO,   KC_NO,            KC_TRNS,          KC_0,              KC_NO,   KC_NO,   KC_NO,                     KC_LEFT, KC_DOWN, KC_RIGHT
-  ),
-
-  [_FN] = LAYOUT_all(
-    QK_BOOT,   KC_BRID, KC_BRIU, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,            KC_SLEP,
-    KC_NO,     KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,                    KC_NO,
-    KC_NO,     KC_VOLD, KC_MUTE, KC_VOLU, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,                    KC_UP,   KC_TRNS,
-    KC_NO,     KC_NO,   KC_NO,            KC_NO,            RGB_TOG,           KC_NO,   KC_NO,   KC_NO,                     KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 };
 
--- dot_config/qmk/keyboards/cannonkeys/sagittarius/keymaps/custom/keymap.c
+++ dot_config/qmk/keyboards/cannonkeys/sagittarius/keymaps/custom/keymap.c
@@ -6,37 +6,43 @@ enum layer_names {
     _FN,
 };
 
-#define HYP_SLSH HYPR_T(KC_SLSH)  // Hyper on hold, `/` on tap
-#define HYP_Z    HYPR_T(KC_Z)     // Hyper on hold, `z` on tap
-#define SPC_NUM  LT(_NUM, KC_SPC) // Space on tap, Num layer on hold
+#define TAB_NAV  KC_TAB           // Navigation layer (karabiner)
+#define ESC_CODE KC_CAPS          // Coding layer (karabiner)
+#define HYP_Z    RCMD_T(KC_Z)     // Hyper on hold, `z` on tap
+#define OPT_BSP  LOPT(KC_BSPC)    // Option + Backspace
+// #define MO_NUM  KC_F15
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_default(
-        KC_NO,   QK_GESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_NO,   KC_NO,
-        KC_NO,   KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_NO,   KC_CAPS,         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_NO,   KC_ENT,  KC_NO,
-        MO(_FN), KC_LSFT, KC_NO,  HYP_Z,   KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  HYP_SLSH, KC_RSFT, KC_RSFT, MO(_FN),
-                 KC_LCTL, KC_LOPT,                        KC_LCMD, SPC_NUM,         KC_SPC,  KC_NO,                     KC_HYPR,  KC_NO,   KC_ROPT
+        KC_NO,   QK_GESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_NO,
+        KC_NO,   TAB_NAV,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, OPT_BSP, KC_BSLS,
+        KC_NO,   ESC_CODE,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,  KC_NO,
+        KC_NO,   KC_LSFT, KC_NO,  HYP_Z,   KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_NO,   MO(_FN),
+                 KC_LCTL, KC_LOPT,                        KC_LCMD, MO(_NUM),        KC_SPC,  KC_NO,                     KC_HYPR, KC_NO,   KC_ROPT
     ),
 
     [_NUM] = LAYOUT_default(
-        KC_NO,   KC_NO,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,    KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_7,    KC_8,    KC_9,    KC_PLUS, KC_NO,    KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,           KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_NO,        KC_4,    KC_5,    KC_6,    KC_MINS, KC_MINS,  KC_NO,   KC_NO,   KC_ENT,  KC_NO,
-        KC_NO,   KC_NO,  KC_NO,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_NO,        KC_1,    KC_2,    KC_3,    KC_DOT,  KC_SLSH,  KC_NO,   KC_NO,   KC_NO,
-                 KC_NO,  KC_NO,                              KC_NO, KC_TRNS,        KC_0,    KC_NO,                               KC_NO,   KC_NO,   KC_NO
+        KC_NO,   KC_NO,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS, KC_TRNS, KC_NO,
+        KC_NO,   KC_NO,           KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_NO,        KC_NO,   KC_4,    KC_5,    KC_6,    KC_NO,   KC_NO,   KC_NO,   KC_ENT,  KC_NO,
+        KC_NO,   KC_NO,  KC_NO,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_NO,        KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,  KC_NO,                              KC_NO, KC_TRNS,        KC_0,    KC_NO,                              KC_NO,   KC_NO,   KC_NO
     ),
 
     [_FN] = LAYOUT_default(
-        QK_BOOT, KC_NO,           KC_BRID, KC_BRIU, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_SLEP,
-        KC_NO,   KC_NO,           KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,        RGB_VAD, RGB_VAI, RGB_M_P, RGB_M_B, RGB_M_R,  KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,           KC_VOLD, KC_MUTE, KC_VOLU, KC_NO,   KC_NO,        RGB_HUD, RGB_HUI, RGB_M_SW,RGB_M_SN,RGB_M_K,  KC_NO,   KC_NO,   RGB_MOD, KC_NO,
-        KC_TRNS, KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        RGB_SAD, RGB_SAI, RGB_M_X, RGB_M_G, KC_NO,    KC_NO,   KC_NO,   KC_TRNS,
-                 KC_NO,  KC_NO,                              KC_NO,   KC_NO,        RGB_TOG, KC_NO,                               KC_NO,   KC_NO,   KC_NO
+        QK_BOOT, KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_SLEP,
+        KC_NO,   KC_NO,           KC_BRID, KC_BRIU, KC_NO,   KC_NO,   KC_NO,        RGB_VAD, RGB_VAI, RGB_M_P, RGB_M_B, RGB_M_R, KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,           KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,        RGB_HUD, RGB_HUI, RGB_M_SW,RGB_M_SN,RGB_M_K, KC_NO,   KC_NO,   RGB_MOD, KC_NO,
+        KC_NO,   KC_NO,  KC_NO,   KC_VOLD, KC_MUTE, KC_VOLU, KC_NO,   KC_NO,        RGB_SAD, RGB_SAI, RGB_M_X, RGB_M_G, KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+                 KC_NO,  KC_NO,                              KC_NO,   KC_NO,        RGB_TOG, KC_NO,                              KC_NO,   KC_NO,   KC_NO
     ),
 };
 
-// KC_KB_POWER
-// KC_SYSTEM_POWER
-// KC_SYSTEM_SLEEP
-// KC_SYSTEM_WAKE
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case RCMD_T(KC_Z):
+            return 30; // in ms
+        default:
+            return 100;
+    }
+};

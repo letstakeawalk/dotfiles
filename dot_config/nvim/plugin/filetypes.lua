eedc2e6f0f4a29455e62499d4a73addc5a0d2f2d
Ryan Byon <ryan.h.byon@gmail.com>
2025-01-09 13:25:55 -0800

    feat(ghostty): Ghostty

--- dot_config/nvim/plugin/filetypes.lua
+++ dot_config/nvim/plugin/filetypes.lua
@@ -6,6 +6,7 @@ vim.filetype.add({
     pattern = {
         [".*%.toml%.tmpl"] = "toml",
         [".*/git/config"] = "gitconfig",
+        [".*/ghostty/config.*"] = "config",
         -- ["%.env%..*"] = "sh",
     },
 })

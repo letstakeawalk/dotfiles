e29ccc24221bccab3368cfeb0a2b73827e0566bc
Ryan Byon <ryan.h.byon@gmail.com>
2025-06-11 01:00:34 -0700

    feat: New tmux and vim navigation key bindings

--- dot_config/nvim/lua/plugins/tmux.lua
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -1,10 +1,10 @@
 return {
     "aserowy/tmux.nvim",
     keys = {
-        { "<C-j>", [[<cmd>lua require("tmux").move_left()<cr>]], desc = "Tmux Navigate Left" },
-        { "<C-k>", [[<cmd>lua require("tmux").move_bottom()<cr>]], desc = "Tmux Navigate Down" },
-        { "<C-h>", [[<cmd>lua require("tmux").move_top()<cr>]], desc = "Tmux Navigate Up" },
-        { "<C-l>", [[<cmd>lua require("tmux").move_right()<cr>]], desc = "Tmux Navigate Right" },
+        { "<C-w>j", [[<cmd>lua require("tmux").move_left()<cr>]],   desc = "Tmux Navigate Left" },
+        { "<C-w>k", [[<cmd>lua require("tmux").move_bottom()<cr>]], desc = "Tmux Navigate Down" },
+        { "<C-w>h", [[<cmd>lua require("tmux").move_top()<cr>]],    desc = "Tmux Navigate Up" },
+        { "<C-w>l", [[<cmd>lua require("tmux").move_right()<cr>]],  desc = "Tmux Navigate Right" },
     },
     opts = {
         navigation = {

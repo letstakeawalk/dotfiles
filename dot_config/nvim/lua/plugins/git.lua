55a37c94ea6bc1bc1b8e318f2eb00ca256818330
Ryan Byon <ryan.h.byon@gmail.com>
2025-08-14 15:04:43 -0700

    feat(nvim): added git-conflict plugin

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -162,6 +162,20 @@ return {
         },
     },
     {
+        "akinsho/git-conflict.nvim",
+        version = "*",
+        opts = {
+            default_mappings = true, -- disable buffer local mapping created by this plugin
+            default_commands = true, -- disable commands created by this plugin
+            disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+            list_opener = "copen", -- command or function to open the conflicts list
+            highlights = { -- They must have background color, otherwise the default color will be used
+                incoming = "DiffAdd",
+                current = "DiffText",
+            },
+        },
+    },
+    {
         "ThePrimeagen/git-worktree.nvim",
         keys = {
             { "<leader>w", "<cmd>Telescope git_worktree git_worktrees<cr>", desc = "Git worktrees (Telescope)" },

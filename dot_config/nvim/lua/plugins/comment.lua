d7ce59c5bde17574690666889e6f7378291f479e
Ryan Byon <ryan.h.byon@gmail.com>
2025-08-28 19:32:35 -0700

    refactor(nvim): Improve comment and yank-paste workflow
    This commit refactors the configurations for yanky.nvim and
    comment.nvim to improve usability and lazy-loading.
    
    Key changes include:
    - Switched yanky.nvim to use indent-preserving paste actions.
    - Updated comment.nvim keymaps to allow yanking before commenting in
      visual mode.
    - Removed explicit `event = "BufRead"` to rely on lazy.nvim's key-based
      triggers, improving startup time.
    - Updated CopilotChat.nvim from the canary to the main branch.
    - Removed the unused nvim-jdtls plugin.

--- dot_config/nvim/lua/plugins/comment.lua
+++ dot_config/nvim/lua/plugins/comment.lua
@@ -5,22 +5,19 @@ return {
         "nvim-treesitter/nvim-treesitter",
         "JoosepAlviste/nvim-ts-context-commentstring",
     },
-    event = "BufRead",
     config = function()
         require("Comment").setup({
             pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
             extra = {
                 eol = "gca",
             },
+            mappings = { basic = true, extra = true },
         })
         -- stylua: ignore start
+        vim.keymap.set("n", "gcc", "<Plug>(comment_toggle_linewise_current)",   { desc = "Comment toggle current line" })
+        vim.keymap.set("n", "gcy", "yy<Plug>(comment_toggle_linewise_current)", { desc = "Yank & Comment toggle current line" })
         vim.keymap.set("x", "gc",  "<Plug>(comment_toggle_linewise_visual)",    { desc = "Comment toggle linewise" })
-        vim.keymap.set("n", "gcc", "<Plug>(comment_toggle_linewise_current)",   { desc = "Yank & Comment toggle current line" })
-        vim.keymap.set("x", "gy",  "gv<Plug>(comment_toggle_linewise_visual)",  { desc = "Yank & Comment toggle linewise" })
-        vim.keymap.set("n", "gcy", "yy<Plug>(comment_toggle_linewise_current)", { desc = "Comment toggle current line" })
+        vim.keymap.set("x", "gy",  "ygv<Plug>(comment_toggle_linewise_visual)", { desc = "Comment toggle linewise" })
         -- stylua: ignore end
-        -- vim.keymap.set("x", "gc",  "<Plug>(YankyYank)|gv<Plug>(comment_toggle_linewise_visual)", { desc = "Comment toggle linewise" })
-        -- vim.keymap.set("n", "gcc", "yy<Plug>(comment_toggle_linewise_current)", { desc = "Yank & Comment toggle current line" })
-        -- vim.keymap.set("x", "gc", "<Plug>(YankyYank)|gv<Plug>(comment_toggle_linewise_visual)", { desc = "Yank & Comment toggle linewise" })
     end,
 }
--- dot_config/nvim/lua/plugins/yanky.lua
+++ dot_config/nvim/lua/plugins/yanky.lua
@@ -1,15 +1,14 @@
 return {
     "gbprod/yanky.nvim",
-    event = "BufRead",
     -- stylua: ignore
     keys = {
-        { "y", "<Plug>(YankyYank)",               { "n", "x" }, desc = "Yank" },
-        { "p", "<Plug>(YankyPutAfter)",           { "n", "x" }, desc = "Paste After" },
-        { "P", "<Plug>(YankyPutBefore)",          { "n", "x" }, desc = "Paste Before" },
-        { "gp", "<Plug>(YankyGPutAfter)",         { "n", "x" }, desc = "Paste After" },
-        { "gP", "<Plug>(YankyGPutBefore)",        { "n", "x" }, desc = "Paste Before" },
-        { "]p", "<Plug>(YankyPutAfterLinewise)",  { "n", "x" }, desc = "Paste Below" },
-        { "[p", "<Plug>(YankyPutBeforeLinewise)", { "n", "x" }, desc = "Paste Above" },
+        { "y", "<Plug>(YankyYank)",                     desc = "Yank",         mode = { "n", "x" } },
+        { "p", "<Plug>(YankyPutAfter)",                 desc = "Paste After",  mode = { "n", "x" } },
+        { "P", "<Plug>(YankyPutBefore)",                desc = "Paste Before", mode = { "n", "x" } },
+        { "gp", "<Plug>(YankyGPutAfter)",               desc = "Paste After",  mode = { "n", "x" } },
+        { "gP", "<Plug>(YankyGPutBefore)",              desc = "Paste Before", mode = { "n", "x" } },
+        { "]p", "<Plug>(YankyPutIndentAfterLinewise)",  desc = "Paste Below",  mode = { "n", "x" } },
+        { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Paste Above",  mode = { "n", "x" } },
     },
     opts = {
         ring = {
@@ -19,8 +18,8 @@ return {
             on_put = false,
             on_yank = false,
         },
-    },
-    preserve_cursor_position = {
-        enabled = false,
+        preserve_cursor_position = {
+            enabled = false,
+        },
     },
 }

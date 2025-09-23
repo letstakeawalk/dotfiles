9eab1411e5722e66406c9a16aa23b78971fde250
Ryan Byon <ryan.h.byon@gmail.com>
2025-09-23 03:23:17 -0700

    feat(theme): add nord themes for vivid and eza, update zsh config
    - Add nord color theme for vivid at dot_config/vivid/themes/nord.yml
    - Add nord color theme for eza at dot_config/eza/theme.yml
    - Update nord color palette in Neovim Lua config
    - Set EZA_CONFIG_DIR and LS_COLORS in zshrc for vivid/eza integration
    - Refine zsh completion styles to use vivid-generated LS_COLORS

--- dot_config/nvim/lua/nord/init.lua
+++ dot_config/nvim/lua/nord/init.lua
@@ -1,26 +1,29 @@
 -- stylua: ignore start
 
 local M = {
-    c00d_black  = "#272C36", -- #272C36 --
-    c00_black   = "#2E3440", -- #2E3440 -- background
-    c00b_black  = "#353C4A", -- #353C4A -- statusline, folded
-    c01_gray    = "#3B4252", -- #3B4252 -- cursor line
-    c02_gray    = "#434C5E", -- #434C5E -- visual selection
-    c03_gray    = "#4C566A", -- #4C566A --
-    c03b_gray   = "#616E88", -- #616E88 -- comment, float border, conceal
-    c04d_white  = "#98A2B5", -- #98A2B5 -- unmatched fuzzy result
-    c04_white   = "#D8DEE9", -- #D8DEE9 -- foreground
-    c05_white   = "#E5E9F0", -- #E5E9F0 --
-    c06_white   = "#ECEFF4", -- #ECEFF4 --
-    c07_teal    = "#8FBCBB", -- #8FBCBB --
-    c08_cyan    = "#88C0D0", -- #88C0D0 -- function
-    c09_glacier = "#81A1C1", -- #81A1C1 -- keyword, info
-    c10_blue    = "#5E81AC", -- #5E81AC -- hint, cursearch
-    c11_red     = "#BF616A", -- #BF616A -- error
-    c12_orange  = "#D08770", -- #D08770 --
-    c13_yellow  = "#EBCB8B", -- #EBCB8B -- warn
-    c14_green   = "#A3BE8C", -- #A3BE8C -- string
-    c15_purple  = "#B48EAD", -- #B48EAD -- number
+    c00d_black   = "#272C36", -- #272C36 --
+    c00_black    = "#2E3440", -- #2E3440 -- background
+    c00b_black   = "#353C4A", -- #353C4A -- statusline, folded
+    c01_gray     = "#3B4252", -- #3B4252 -- cursor line
+    c02_gray     = "#434C5E", -- #434C5E -- visual selection
+    c03_gray     = "#4C566A", -- #4C566A --
+    c03b_gray    = "#616E88", -- #616E88 -- comment, float border, conceal
+    c04d_white   = "#98A2B5", -- #98A2B5 -- unmatched fuzzy result
+    c04_white    = "#D8DEE9", -- #D8DEE9 -- foreground
+    c05_white    = "#E5E9F0", -- #E5E9F0 --
+    c06_white    = "#ECEFF4", -- #ECEFF4 --
+    c04_offwhite = "#E9E3D8", -- #E9E3D8
+    c05_offwhite = "#F0ECE5", -- #F0ECE5
+    c06_offwhite = "#F4F1EC", -- #F4F1EC
+    c07_teal     = "#8FBCBB", -- #8FBCBB --
+    c08_cyan     = "#88C0D0", -- #88C0D0 -- function
+    c09_glacier  = "#81A1C1", -- #81A1C1 -- keyword, info
+    c10_blue     = "#5E81AC", -- #5E81AC -- hint, cursearch
+    c11_red      = "#BF616A", -- #BF616A -- error
+    c12_orange   = "#D08770", -- #D08770 --
+    c13_yellow   = "#EBCB8B", -- #EBCB8B -- warn
+    c14_green    = "#A3BE8C", -- #A3BE8C -- string
+    c15_purple   = "#B48EAD", -- #B48EAD -- number
 
     c07d_jade    = "#729696", -- #729696
     c08d_cyan    = "#6D9AA6", -- #6D9AA6
@@ -43,6 +46,7 @@ local M = {
     c13b_yellow  = "#EFD5A2", -- #EFD5A2
     c14b_green   = "#B5CBA3", -- #B5CBA3
     c15b_purple  = "#C3A5BD", -- #C3A5BD
+
 }
 
 M.bg         = M.c00_black

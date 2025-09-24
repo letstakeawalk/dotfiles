e3e2e0a8032ba6e9c432120ea517549879b8f97b
Ryan Byon <ryan.h.byon@gmail.com>
2025-09-24 08:47:41 -0700

    refactor(nvim): cleanup lspconfigs

--- dot_config/nvim/lsp/jsonls.lua
+++
@@ -1,8 +0,0 @@
-return {
-    cmd = { "vscode-json-language-server", "--stdio" },
-    filetypes = { "json", "jsonc" },
-    init_options = {
-        provideFormatter = false,
-    },
-    root_markers = { ".git" },
-}
--- dot_config/nvim/lsp/pyright.lua
+++ dot_config/nvim/lsp/pyright.lua
@@ -1,51 +1,4 @@
----@diagnostic disable: param-type-mismatch
----@brief
----
---- https://github.com/microsoft/pyright
----
---- `pyright`, a static type checker and language server for python
-
--- pyright = {
---     settings = {
---         python = {
---             analysis = {
---                 ignore = { "*" }, -- let ruff handle linting
---                 -- typeCheckingMode = "off", -- let mypy handle type-checking
---                 diagnosticSeverityOverrides = { reportUndefinedVariable = "none" }, -- "error," "warning," "information," "true," "false," or "none"
---             },
---         },
---         -- pyright = { disableOrganizeImports = true },
---     },
--- }
-
-local function set_python_path(path)
-    local clients = vim.lsp.get_clients({
-        bufnr = vim.api.nvim_get_current_buf(),
-        name = "pyright",
-    })
-    for _, client in ipairs(clients) do
-        if client.settings then
-            client.settings.python = vim.tbl_deep_extend("force", client.settings.python, { pythonPath = path })
-        else
-            client.config.settings =
-                vim.tbl_deep_extend("force", client.config.settings, { python = { pythonPath = path } })
-        end
-        client:notify("workspace/didChangeConfiguration", { settings = nil })
-    end
-end
-
 return {
-    cmd = { "pyright-langserver", "--stdio" },
-    filetypes = { "python" },
-    root_markers = {
-        "pyproject.toml",
-        "setup.py",
-        "setup.cfg",
-        "requirements.txt",
-        "Pipfile",
-        "pyrightconfig.json",
-        ".git",
-    },
     settings = {
         python = {
             analysis = {
@@ -55,19 +8,4 @@ return {
             },
         },
     },
-    on_attach = function(client, bufnr)
-        vim.api.nvim_buf_create_user_command(bufnr, "LspPyrightOrganizeImports", function()
-            client:exec_cmd({
-                command = "pyright.organizeimports",
-                arguments = { vim.uri_from_bufnr(bufnr) },
-            })
-        end, {
-            desc = "Organize Imports",
-        })
-        vim.api.nvim_buf_create_user_command(bufnr, "LspPyrightSetPythonPath", set_python_path, {
-            desc = "Reconfigure pyright with the provided python path",
-            nargs = 1,
-            complete = "file",
-        })
-    end,
 }

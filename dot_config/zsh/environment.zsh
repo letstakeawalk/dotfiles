cb01858a86e562b4d7f8ea3c8508ddf3898a3f03
Ryan Byon <ryan.h.byon@gmail.com>
2025-09-27 21:47:13 -0700

    feat(env): set `LESSHISTFILE` to XDG data directory

--- dot_config/zsh/environment.zsh
+++ dot_config/zsh/environment.zsh
@@ -10,6 +10,7 @@ export GOKU_EDN_CONFIG_FILE=$XDG_CONFIG_HOME/goku/karabiner.edn
 export GOPATH=$XDG_DATA_HOME/go
 export HOMEBREW_BUNDLE_FILE=$XDG_CONFIG_HOME/brewfile/Brewfile
 export LESS="-R --lesskey-src=$XDG_CONFIG_HOME/less/lesskey"
+export LESSHISTFILE=$XDG_DATA_HOME/less_history
 export MISE_CACHE_DIR=$XDG_CACHE_HOME/mise
 export MISE_CONFIG_DIR=$XDG_CONFIG_HOME/mise
 export MISE_DATA_DIR=$XDG_DATA_HOME/mise

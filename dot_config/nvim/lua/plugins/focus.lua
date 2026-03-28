514162f6480ad5bea4f8c21e21988245f9a35398
Ryan Byon <ryan.h.byon@gmail.com>
2026-03-28 10:52:08 -0700

    feat(nvim): focus.nvim

--- dot_config/nvim/lua/plugins/focus.lua
+++ dot_config/nvim/lua/plugins/focus.lua
@@ -1,11 +1,14 @@
 return {
     "nvim-focus/focus.nvim",
     event = "VeryLazy",
-    keys = { "<leader>df", "<cmd>FocusToggle<cr>", desc = "Focus Toggle" },
+    keys = { { "<leader>df", "<cmd>FocusToggle<cr>", desc = "Focus Toggle" } },
     opts = {
         ui = {
             signcolumn = false,
             cursorline = false,
+        },
+        autoresize = {
+            height_quickfix = 10, -- Set the height of quickfix panel
         },
     },
 }
